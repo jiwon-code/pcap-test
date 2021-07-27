8cfc9591f85d297cf367846f90077a00e1c810d9
jiwon-code <bani001019@gmail.com>
2021-07-27 13:09:34 -0700

    Add makefile

--- pcap-test.c
+++ pcap-test.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define _CRT_SECURE_NO_WARNINGS
+
 void usage() {
 	printf("syntax: pcap-test <interface>\n");
 	printf("sample: pcap-test wlan0\n");
@@ -11,7 +13,7 @@ typedef struct {
 	char* dev_;
 } Param;
 
-Param param  = {
+Param ens33  = {
 	.dev_ = NULL
 };
 
@@ -25,13 +27,13 @@ bool parse(Param* param, int argc, char* argv[]) {
 }
 
 int main(int argc, char* argv[]) {
-	if (!parse(&param, argc, argv))
+    if (!parse(&ens33, argc, argv))
 		return -1;
 
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+    pcap_t* pcap = pcap_open_live(ens33.dev_, BUFSIZ, 1, 1000, errbuf);
 	if (pcap == NULL) {
-		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", ens33.dev_, errbuf);
 		return -1;
 	}
 
@@ -44,8 +46,34 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
-		printf("%u bytes captured\n", header->caplen);
+        printf("%u bytes captured\n", header->caplen);
+        printf("src mac : %02X %02X %02X %02X %02X %02X\n",packet[6],packet[7],packet[8],packet[9],packet[10],packet[11]);
+        printf("dst mac : %02X %02X %02X %02X %02X %02X\n",packet[0],packet[1],packet[2],packet[3],packet[4],packet[5]);
+        if ((packet[12]==0x08)&&(packet[13]==0x00)){
+            printf("IPv4\n");
+            printf("src ip : %02X %02X %02X %02X\n",packet[26],packet[27],packet[28],packet[29]);
+            printf("dst ip : %02X %02X %02X %02X\n",packet[30],packet[31],packet[32],packet[33]);
+            if (packet[23]==0x06){
+                printf("TCP\n");
+                printf("src port : %02X %02X\n", packet[33], packet[34]);
+                printf("dst port : %02X %02X\n", packet[35], packet[36]);
+                //if TCP data > 8,
+            }
+            else {
+                printf("Not TCP\n");
+            }
+        }
+        else if((packet[12]==0x08)&&(packet[13]==0x06)){
+            printf("ARP\n");
+        }
+        else{
+            printf("else\n");
+        }
+        printf("========================================================================");
+
+
+
 	}
 
-	pcap_close(pcap);
+    pcap_close(pcap);
 }
